53521b010641e1fb0de30e81f69d5539a119f977
mitrich83 <dmitry.v.malykhin@gmail.com>
2021-07-26 20:51:23 +0300

    5 task added css for HW2, HW3; also added input and button component

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import s from './pages/PreJunior.module.css'
+import s from './pages/Header.module.css'
 
 
 function Header() {
 
     return (
         <div className={s.div}>
-            <NavLink className={s.preJunior}  to={'pre-junior'}>| pre-junior |</NavLink>
-            <NavLink className={s.junior} to={'junior'}> junior </NavLink>
-            <NavLink className={s.juniorPlus} to={'junior-plus'}>| junior+ |</NavLink>
+            <NavLink className={s.preJunior} activeClassName={s.active}  to={'pre-junior'}>| pre-junior |</NavLink>
+            <NavLink className={s.junior} activeClassName={s.active} to={'junior'}> junior </NavLink>
+            <NavLink className={s.juniorPlus} activeClassName={s.active} to={'junior-plus'}>| junior+ |</NavLink>
             <button className={s.button}></button>
         </div>
     )
